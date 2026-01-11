d20c0f8b35c92156de0c04b55c15537d953cdd12
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2026-01-11 18:24:41 +0200

    osa 2 - funktio

--- Week1/ekaCPP_project/main.cpp
+++ Week1/ekaCPP_project/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int game(){
     std::srand(time(0));
     int randNumber;
     int inputNumber;
     int gameStatus=0;
+    int gussCount=0;
 
     randNumber = rand() % 20;
 
@@ -15,6 +16,7 @@ int main(){
         gameStatus=1;
         cout << "Enter Gussed Number: " << endl;
         cin >> inputNumber;
+        gussCount++;
 
         if(inputNumber>randNumber){
             cout << "liian suuri" << endl;
@@ -25,4 +27,11 @@ int main(){
             cout << "oikein" << endl;
         }
     }while(gameStatus==1);
+
+    return gussCount;
+}
+
+int main(){
+    int count = game();
+    cout << "eli arvauskertojen lukumäärää : " << count << endl;
 }
