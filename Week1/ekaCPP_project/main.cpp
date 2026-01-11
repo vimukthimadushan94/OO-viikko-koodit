5fa30928b83c41e6de4ac56a1cfbf68e07ad32f2
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2026-01-11 18:03:11 +0200

    Osa 1 perusversio

--- Week1/ekaCPP_project/main.cpp
+++ Week1/ekaCPP_project/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main(){
+    std::srand(time(0));
+    int randNumber;
+    int inputNumber;
+    int gameStatus=0;
+
+    randNumber = rand() % 20;
+
+
+    do{
+        //cout << randNumber << endl;
+        gameStatus=1;
+        cout << "Enter Gussed Number: " << endl;
+        cin >> inputNumber;
+
+        if(inputNumber>randNumber){
+            cout << "liian suuri" << endl;
+        }else if(inputNumber<randNumber){
+            cout << "liian pieni" << endl;
+        }else{
+            gameStatus = 0;
+            cout << "oikein" << endl;
+        }
+    }while(gameStatus==1);
 }
